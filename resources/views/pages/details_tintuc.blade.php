51531e2dd11235514129c283cad1029532b0e977
lvan138 <lvan1381996@gmail.com>
2017-07-03 22:48:28 +0700

    plugin fb

--- resources/views/pages/details_tintuc.blade.php
+++ resources/views/pages/details_tintuc.blade.php
@@ -55,7 +55,7 @@
 	  					<li id="plug">
 	  						<iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&width=154&layout=button_count&action=like&size=small&show_faces=true&share=true&height=46&appId" width="154" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
 	  					</li>
-	  					<i class="fa fa-pencil" aria-hidden="true"> Bo khoa hoc Cong nghe va phat trien</i>
+	  					<i class="fa fa-pencil" aria-hidden="true"> Bộ khoa học và Công nghệ</i>
 					</ul>
 		    	</div>
 		    	<div class="people-write">
