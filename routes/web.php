73c3b176058921e624009b77640a1ef6f8d7fae4
cuongvumanh <borugairu.love@gmail.com>
2017-07-11 16:34:47 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -34,26 +34,26 @@ Route::get('/','PageController\PageController@TrangChu')->name('home');
 //Route::get('tintuc','PageController\PageController@TinTuc');
 Route::get('lien-he','PageController\PageController@LienHe');
 Route::get('detail','PageController\PageController@Detail');
-Route::get('cauhoithuonggap','PageController\PageController@Cauhoithuonggap');
+Route::get('cau-hoi-thuong-gap','PageController\PageController@Cauhoithuonggap');
 
 Route::post('/change-language','SessionController@changeLanguage');
 
-Route::get('gioithieuchung','PageController\PageController@GioiThieuChung');
-Route::get('vitrichucnang','PageController\PageController@ViTriChucNang');
-Route::get('sumenhtamnhin','PageController\PageController@SuMenhTamNhin');
-Route::get('doingutrungtam','PageController\PageController@DoiNguTrungTam');
-Route::get('cocau','PageController\PageController@CoCau');
-Route::get('chuyengia','PageController\PageController@ChuyenGia');
+Route::get('gioi-thieu-chung','PageController\PageController@GioiThieuChung');
+Route::get('vi-tri-chuc-nang','PageController\PageController@ViTriChucNang');
+Route::get('su-menh-tam-nhin','PageController\PageController@SuMenhTamNhin');
+Route::get('doi-ngu-trung-tam','PageController\PageController@DoiNguTrungTam');
+Route::get('co-cau','PageController\PageController@CoCau');
+Route::get('chuyen-gia','PageController\PageController@ChuyenGia');
 
-Route::get('tuyendung','PageController\PageController@TuyenDung');
-Route::get('tuyendung/{slug}','PageController\PageController@DetailsTuyenDung');
+Route::get('tuyen-dung','PageController\PageController@TuyenDung');
+Route::get('tuyen-dung/{slug}','PageController\PageController@DetailsTuyenDung');
 // show all news of all of kind
-Route::get('tintuc','PageController\TinTucController@allNews');
+Route::get('tin-tuc','PageController\TinTucController@allNews');
 // show all news of  a kind
-Route::get('tintuc/{slug}','PageController\TinTucController@newsOfKind');
+Route::get('tin-tuc/{slug}','PageController\TinTucController@newsOfKind');
 //show detail one new
-Route::get('tintuc/{slug_loai_tin}/{slug_tin_tuc}','PageController\TinTucController@detailsNew');
+Route::get('tin-tuc/{slug_loai_tin}/{slug_tin_tuc}','PageController\TinTucController@detailsNew');
 //show all su kien
 
-Route::get('sukien','PageController\SuKienController@danhSachSuKien');
-Route::get('sukien/{slug}','PageController\SuKienController@detailsSuKien');
+Route::get('su-kien','PageController\SuKienController@danhSachSuKien');
+Route::get('su-kien/{slug}','PageController\SuKienController@detailsSuKien');
